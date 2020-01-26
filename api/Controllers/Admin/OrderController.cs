48bc513b11497a4aa586f09cb14fa0dcf93b985c
pr.jereczek <pr.jereczek@gmail.com>
2020-01-26 21:31:26 +0100

    changing order status and listing all orders

--- api/Controllers/Admin/OrderController.cs
+++ api/Controllers/Admin/OrderController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using api.Models;
+using api.Dto;
+using Newtonsoft.Json;
 
 namespace api.Controllers.Admin
 {
@@ -19,5 +21,38 @@ namespace api.Controllers.Admin
             _context = context;
         }
 
+        [HttpGet]
+        public IActionResult getAllOrders()
+        {
+            return Ok(_context.Order.ToList().Select(order =>
+            {
+                var orderDTO = new OrderDTO();
+                orderDTO.Uid = order.Uid;
+                orderDTO.Phone = order.Phone;
+                orderDTO.Price = order.Price;
+                orderDTO.PizzaDefinition = JsonConvert.DeserializeObject<OrderDefinitionDTO>(order.PizzaDefinition);
+                orderDTO.OrderStatus = (Dto.OrderStatus)Enum.Parse(typeof(Dto.OrderStatus), order.OrderStatusName);
+                return orderDTO;
+            }));
+        }
+
+        [HttpPatch("{uid}/status")]
+        public IActionResult changeOrderStatus(String uid, [FromQuery(Name = "newStatus")] Dto.OrderStatus newStatus)
+        {
+            var order = _context.Order.FirstOrDefault(it => it.Uid == uid);
+            if(order == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                order.OrderStatusName = newStatus.ToString();
+                _context.SaveChanges();
+            }
+            return Ok();
+        }
+
+
     }
+
 }
